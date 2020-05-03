b007b9f217fa6a32594ce9c0d71d22dc7401518a
Jose Pitti <japittic@gmail.com>
2020-05-02 23:44:22 -0500

    Commit Inicial

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('./config/config');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+
+app.get('/usuario', function(req, res) {
+    res.json('get Usuario');
+});
+
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+
+    if (body.nombre === undefined) {
+        res.status(400).json({
+            ok: false,
+            mensaje: 'El nombre es necesario'
+        });
+
+
+
+    } else {
+        res.json({
+            body: body,
+            nombre: "Juan de Dios"
+        });
+    }
+
+});
+
+
+
+app.put('/usuario/:id', function(req, res) {
+
+    let id = req.params.id;
+    res.json({
+        id: id
+    });
+});
+
+app.delete('/usuario', function(req, res) {
+    res.json('delete Usuario');
+});
+
+app.listen(process.env.PORT);
+console.log('Escuchando el puerto 3000');
