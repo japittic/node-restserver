477d882b366582608f3c78d85d8bf02aa2502e3e
Jose Pitti <japittic@gmail.com>
2020-05-06 22:08:52 -0500

    Rest Server2

--- server/config/config.js
+++ server/config/config.js
@@ -7,8 +7,12 @@ if (process.env.NODE_ENV === 'dev') {
 
     urlDb = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDb = process.env.MONGO_URI;
+    // urlDb = process.env.MONGO_URI;
+    urlDb = 'mongodb+srv://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net/test?retryWrites=true&w=majority'
 }
 // urlDb = 'mongodb+srv://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net/cafe'
+// "mongodb://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net:27017/cafe"
+// mongodb://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net:27017/cafe
+// const uri = "mongodb+srv://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net/test?retryWrites=true&w=majority";
 
 process.env.URLDB = urlDb;
