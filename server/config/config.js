a37eb81635d921b7ce82e3f4988c87f63e006629
Jose Pitti <japittic@gmail.com>
2020-05-10 23:13:54 -0500

    Login Normal Seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.Caducidad_token = 60 * 60 * 24 * 30;
 // let mitoken;
 process.env.seed_token = 'secret';
-let seed;
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 process.env.seed = process.env.seed || 'secret';
@@ -14,8 +14,8 @@ if (process.env.NODE_ENV === 'dev') {
     urlDb = 'mongodb://localhost:27017/cafe'
 } else {
     // urlDb = process.env.MONGO_URI;
-    urlDb = 'mongodb://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net:13209/cafe'
-
+    // urlDb = 'mongodb://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net:13209/cafe'
+    urlDb = 'mongodb://japittic:exJC9awL3VgYKOD8@cluster0-shard-00-01-si9da.mongodb.net:27017/cafe'
 }
 // urlDb = 'mongodb+srv://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net/cafe'
 // "mongodb://japittic:exJC9awL3VgYKOD8@cluster0-si9da.mongodb.net:27017/cafe"
